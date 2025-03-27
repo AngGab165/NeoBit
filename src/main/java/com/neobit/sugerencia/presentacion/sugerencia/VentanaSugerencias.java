ed7dfa089ff622da2a779b1206cc0961194f3699
AngGab165 <cbi2223046760@izt.uam.mx>
2025-03-27 00:23:56 -0600

    Cambios que se visualizan

--- src/main/java/com/neobit/sugerencia/presentacion/sugerencia/VentanaSugerencias.java
+++ src/main/java/com/neobit/sugerencia/presentacion/sugerencia/VentanaSugerencias.java
@@ -1,5 +1,6 @@
 package com.neobit.sugerencia.presentacion.sugerencia;
 
+//Documentacion
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
